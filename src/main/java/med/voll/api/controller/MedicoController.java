578f9bd12e40e92d91b183f7c5248c899a9b4214
pedro.silva <pedro.silva@sensedia.com>
2024-12-25 20:58:29 -0300

    Padronizando o Controller com ResponseEntity para os seus devidos retornos de requisição e acrescentando nova classe dto para detalhamento das informações de um médico.

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -5,12 +5,14 @@ import med.voll.api.controller.dto.DadosAtualizacaoMedicoDTO;
 import med.voll.api.controller.dto.DadosCadastrosMedicosDTO;
 import med.voll.api.controller.dto.DadosListagemMedicosDTO;
 import med.voll.api.endereco.DadosEndereco;
+import med.voll.api.controller.dto.DadosDetalhamentoMedicoDTO;
 import med.voll.api.medico.Medico;
 import med.voll.api.repository.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,34 +29,41 @@ public class MedicoController {
 
     @PostMapping
     @Transactional //metodo de inscrita exige uma transacao ativa com o banco
-    public void cadastrar(@RequestBody @Valid DadosCadastrosMedicosDTO dados) { //valid ira validar o meu objeto "dados" com as anotacoes declaradas nas classes dto
+    public ResponseEntity cadastrar(@RequestBody @Valid DadosCadastrosMedicosDTO dados) { //valid ira validar o meu objeto "dados" com as anotacoes declaradas nas classes dto
 
         medicoRepository.save(new Medico(dados));//converte o parametro dto para um objeto do tipo medico
     }
 
     //metodo de leitura
     @GetMapping                                         //define os padroes de paginacao ao spring caso venha default
-    public Page<DadosListagemMedicosDTO> listarMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
+    public ResponseEntity<Page<DadosListagemMedicosDTO>> listarMedicos(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao){
 
-        return medicoRepository.findAllByAtivoTrue(paginacao).map(DadosListagemMedicosDTO::new); //exibe apenas os medicos com status ativo = 1
+        var page = medicoRepository.findAllByAtivoTrue(paginacao).map(DadosListagemMedicosDTO::new); //exibe apenas os medicos com status ativo = 1
+        return ResponseEntity.ok(page); //devolvendo codigo 200 "ok" passando o objeto "page" junto na resposta
     }
 
     @PutMapping
     @Transactional
     //jpa ao analisar que foi modificado algum campo na transacao, realiza o update sozinha
-    public void atualizar(@RequestBody @Valid DadosAtualizacaoMedicoDTO dados){
+    public ResponseEntity atualizar(@RequestBody @Valid DadosAtualizacaoMedicoDTO dados){
 
         var medico = medicoRepository.getReferenceById(dados.id()); //estou indo no banco e carregando um medico pelo id
         medico.atualizarInformacoes(dados);
+
+        //devolvendo a informação atualizada
+        return ResponseEntity.ok(new DadosDetalhamentoMedicoDTO(medico));
+
     }
 
     //id vem na url {parametro dinamico}
     @DeleteMapping("/{id}")
     @Transactional
-    public void excluirRegistroMedicos(@PathVariable Long id){ // PathVariable = avisando que o id da url e o mesmo do parametro
+    public ResponseEntity excluirRegistroMedicos(@PathVariable Long id){ // PathVariable = avisando que o id da url e o mesmo do parametro
         var medico = medicoRepository.getReferenceById(id); //estou indo no banco e carregando um medico pelo id
         medico.excluir(); //ao chamar o metodo estaremos por padrao definindo o status de ativo como false
-        //medicoRepository.deleteById(id); se acionarmos esse trecho ele ira apagar do banco de dados o id marcado e a ideia e apenas definir como inativo
+
+        return ResponseEntity.noContent().build(); //chama o objeto "noContent", depois constroi o objeto "build" (retorna codigo 204)
+
     }
 
 
